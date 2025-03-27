f2f8fbf1c8390bbe2ebd7e7fcd5b044da55714dc
Svenyx <89574734+Nyxane@users.noreply.github.com>
2025-03-27 09:39:28 -0600

    Remove AE2 Automation

--- kubejs/server_scripts/ars_nouveau/recipes.js
+++ kubejs/server_scripts/ars_nouveau/recipes.js
@@ -4,42 +4,10 @@
 *    As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 */
 ServerEvents.recipes(ATM => {
-    function enchanting_apparatus(output, pedestalItems, reagent, nbt, sourceCost, id) {
-        let recipe = {
-            "type": "ars_nouveau:enchanting_apparatus",
-            "keepNbtOfReagent": nbt,
-            "pedestalItems": [],
-            "reagent": {},
-            "result": {
-                "count": output.count || 1,
-                "id": output.item
-            },
-            "sourceCost": sourceCost
-        };
 
-        if (reagent.tag) {
-            recipe.reagent.tag = reagent.tag;
-        } else {
-            recipe.reagent.item = reagent.item;
-        }
 
-        pedestalItems.forEach(input => {
-
-            let ingredients = {}
-
-            if (input.tag) {
-                ingredients.tag = input.tag;
-            } else {
-                ingredients.item = input.item;
-            }
-
-            recipe.pedestalItems.push(ingredients);
-        });
-
-        ATM.custom(recipe).id(`kubejs:enchanting_apparatus/${id}`);
-    }
-
-    enchanting_apparatus(
+    $ArsNouveau$EnchantingApparatus(
+        ATM,
         { item: 'allthemodium:unobtainium_allthemodium_alloy_ingot' },
         [
             { item: 'primalmagick:hallowsteel_ingot' },
