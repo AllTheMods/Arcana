ae8afa45063a03a3d84a070a050050e57781d2f9
Svenyx <89574734+Nyxane@users.noreply.github.com>
2025-03-27 09:41:32 -0600

    Remove Duplicate Recipe

--- kubejs/server_scripts/arseng/recipes.js
+++ kubejs/server_scripts/arseng/recipes.js
@@ -4,45 +4,6 @@
 *    As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
 */
 ServerEvents.recipes(ATM => {
-    ATM.custom({
-        "type": "ars_nouveau:enchanting_apparatus",
-        "keepNbtOfReagent": false,
-        "pedestalItems": [
-            {
-                "item": "ars_nouveau:source_gem_block"
-            },
-            {
-                "item": "ars_nouveau:source_gem_block"
-            },
-            {
-                "item": "ars_nouveau:source_gem_block"
-            },
-            {
-                "item": "ars_nouveau:source_gem_block"
-            },
-            {
-                "tag": "c:storage_blocks/gold"
-            },
-            {
-                "tag": "c:storage_blocks/gold"
-            },
-            {
-                "tag": "c:storage_blocks/gold"
-            },
-            {
-                "tag": "c:storage_blocks/gold"
-            }
-        ],
-        "reagent": {
-            "item": "ars_nouveau:arcane_core"
-        },
-        "result": {
-            "count": 1,
-            "id": "arseng:source_acceptor"
-        },
-        "sourceCost": 10000
-    })
-    
     $ArsNouveau$EnchantingApparatus(
         ATM,
         Item.of("arseng:source_acceptor"),
