32525b788d71fbd8ebb50b9fc2264ce01a03fac1
thevortex <james.g.bowley@gmail.com>
2025-04-02 08:33:22 -0600

    add magical soil recipe

---
+++ kubejs/server_scripts/mysticalag/recipes.js
@@ -0,0 +1,24 @@
+/*
+*    This File has been authored by AllTheMods Staff, or a Community contributor for use in `All the Magic: Arcana` by ATMTeam.
+*    As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+*/
+ServerEvents.recipes(allthemods => {
+    allthemods.shaped('kubejs:magical_soil', ['ABC', 'DEF', 'GHI'], {
+        A: 'mysticalagradditions:insanium_block',
+        B: 'allthetweaks:nether_star_block',
+        C: 'allthecompressed:dirt_1x',
+        D: 'mysticalagriculture:awakened_supremium_growth_accelerator',
+        E: 'mysticalagradditions:insanium_farmland',
+        F: '#iceandfire:dragon_skulls',
+        G: 'enchanted:oil_of_vitriol',
+        H: 'allthemodium:ancient_dirt',
+        I: 'primalmagick:moonwood_sapling'
+    }).id('allthemods:kjs/magical_soil')
+})
+
+/*
+*    This File has been authored by AllTheMods Staff, or a Community contributor for use in `All the Magic: Arcana` by ATMTeam.
+*    As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+*/
+
+
