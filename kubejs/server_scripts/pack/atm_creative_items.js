8b11880aa26d91e0ae948ca49a350cd9e3b12c46
daestar <43893573+daestar@users.noreply.github.com>
2025-05-14 09:13:05 -0500

    modified creative recipes further

--- kubejs/server_scripts/pack/atm_creative_items.js
+++ kubejs/server_scripts/pack/atm_creative_items.js
@@ -18,13 +18,19 @@ ServerEvents.recipes(ATM => {
         }
     )
 
-    ATM.shaped('ars_nouveau:creative_source_jar', ['AMA', 'ESE', 'GMG'], {
-        G: 'allthemodium:allthemodium_source_jar',
-        S: 'allthetweaks:atm_star',
-        M: 'arsdelight:chimera_horn',
-        A: 'arseng:me_source_jar',
-        E: 'arseng:source_acceptor'
-    })
+
+    ATM.custom({
+        type:"occultism:ritual",
+        result: Item.of("ars_nouveau:creative_source_jar"),
+        ingredients: $Pack$IngredientsOf(["allthemodium:allthemodium_source_jar", "allthemodium:allthemodium_source_jar", 
+            "arsdelight:horn_roll", "arsdelight:horn_roll", 'arseng:me_source_jar', 'arseng:me_source_jar', 'arseng:source_acceptor', 'arseng:source_acceptor']),
+        activation_item: Ingredient.of("allthetweaks:atm_star"),
+        pentacle_id: "occultism:contact_eldritch_spirit",
+        duration: 360,
+        ritual_type: "occultism:craft_miner_spirit",
+        ritual_dummy: Item.of("occultism:ritual_dummy/misc_miner_ancient_eldritch")
+    }).id("kubejs:creative_source_jar")
+
 
 
 //#Create
@@ -52,7 +58,8 @@ ServerEvents.recipes(ATM => {
         ingredients: [
             Ingredient.of('create:blaze_cake').toJson(),
             Ingredient.of('allthetweaks:atm_star').toJson(),
-            Ingredient.of('create_enchantment_industry:experience_cake').toJson()
+            Ingredient.of('create_enchantment_industry:experience_cake').toJson(),
+            Ingredient.of('mahoutsukai:faecake').toJson()
     ],
         results: [Item.of('create:creative_blaze_cake').toJson()],
     })
@@ -77,28 +84,119 @@ ServerEvents.recipes(ATM => {
 
 //#Vampirism
 
-    ATM.shaped('vampirism:sunscreen_beacon',
-        ['GGG','GSG','OCO'],
-        {
-            G: 'minecraft:glass',
-            S: 'allthetweaks:atm_star',
-            O: '#c:obsidians',
-            C: 'vampirism:mother_core'
-        }
-    )
-
-    ATM.shaped('vampiresneedumbrellas:creative_umbrella',
-        [
-            ' US',
-            ' RU',
-            'R  '
+    ATM.custom({
+        "type": "primalmagick:ritual",
+        "expertiseGroup": "primalmagick:ambrosia_supreme",
+        "ingredients": [
+            {
+                "item": "minecraft:glass"
+            },
+            {
+                "item": "allthetweaks:atm_star"
+            },
+            {
+                "tag": "c:obsidians"
+            },
+            {
+                "item": "vampirism:mother_core"
+            },
+            {
+                "item": "vampirism:pure_blood_4"
+            },
+            {
+                "item": "primalmagick:humming_artifact_sun"
+            }
         ],
-        {
-            R: 'alltheores:netherite_rod',
-            U: 'vampiresneedumbrellas:netherite_umbrella',
-            S: 'allthetweaks:atm_star'
+        "instability": 8,
+        "mana": {
+            "sources": {
+                "primalmagick:blood": 30000,
+                "primalmagick:infernal": 30000,
+                "primalmagick:sun": 30000
+            }
+        },
+        "props": [
+            {
+                "tag": "primalmagick:ritual_candles"
+            },
+            {
+                "tag": "primalmagick:ritual_candles"
+            },
+            {
+                "block": "primalmagick:ritual_lectern"
+            },
+            {
+                "block": "primalmagick:bloodletter"
+            }
+        ],
+        "requirement": {
+            "requirement_type": "primalmagick:research",
+            "rootKey": {
+                "key_type": "primalmagick:research_entry",
+                "rootKey": "primalmagick:supreme_ambrosia"
+            }
+        },
+        "result": {
+            "count": 1,
+            "id": "vampirism:sunscreen_beacon"
         }
-    )
+    })
+
+
+    ATM.custom({
+        "type": "primalmagick:ritual",
+        "expertiseGroup": "primalmagick:ambrosia_supreme",
+        "ingredients": [
+            {
+                "item": "alltheores:netherite_rod"
+            },
+            {
+                "item": "alltheores:netherite_rod"
+            },
+            {
+                "item": "allthetweaks:atm_star"
+            },
+            {
+                "item": "vampiresneedumbrellas:netherite_umbrella"
+            },
+            {
+                "item": "vampiresneedumbrellas:netherite_umbrella"
+            }
+        ],
+        "instability": 8,
+        "mana": {
+            "sources": {
+                "primalmagick:blood": 30000,
+                "primalmagick:sun": 30000,
+                "primalmagick:sky": 30000
+            }
+        },
+        "props": [
+            {
+                "tag": "primalmagick:ritual_candles"
+            },
+            {
+                "tag": "primalmagick:ritual_candles"
+            },
+            {
+                "block": "primalmagick:ritual_lectern"
+            },
+            {
+                "block": "primalmagick:bloodletter"
+            }
+        ],
+        "requirement": {
+            "requirement_type": "primalmagick:research",
+            "rootKey": {
+                "key_type": "primalmagick:research_entry",
+                "rootKey": "primalmagick:supreme_ambrosia"
+            }
+        },
+        "result": {
+            "count": 1,
+            "id": "vampiresneedumbrellas:creative_umbrella"
+        }
+    })
 
 })
 
